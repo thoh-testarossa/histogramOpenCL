f8117cba0784359293cf8b723f87a4bb753cbe9d
Thoh Testarossa <huodufashi001@gmail.com>
2017-03-17 16:47:51 +0800

    Modified main.cpp to add some basic OpenCL operators' template

--- histogramOpenCL/main.cpp
+++ histogramOpenCL/main.cpp
@@ -8,6 +8,7 @@
 #endif
 
 #define BUFFERSIZE 1024
+#define BUILDLOGSIZE 16384
 
 using namespace std;
 
@@ -140,6 +141,46 @@ cl_command_queue CreateCommandQueue(cl_context context, cl_device_id *device)
     *device = devices[0];
     delete [] devices;
     return commandQueue;
+}
+
+cl_program CreateProgram(cl_context context, cl_device_id device, const char *fileName)
+{
+    cl_int errNum;
+    cl_program program;
+
+    ifstream kernelFile(fileName, ios::in);
+    if(!kernelFile.is_open())
+    {
+        cerr << "Failed to open file for reading: " << fileName << endl;
+        return NULL;
+    }
+
+    ostringstream oss;
+    oss << kernelFile.rdbuf();
+
+    string srcStdStr = oss.str();
+    const char *srcStr = srcStdStr.c_str();
+    program = clCreateProgramWithSource(context, 1, (const char**)&srcStr, NULL, NULL);
+
+    if(program == NULL)
+    {
+        cerr << "Failed to create CL program from source." << endl;
+        return NULL;
+    }
+
+    errNum = clBuildProgram(program, 0, NULL, NULL, NULL, NULL);
+    if(errNum != CL_SUCCESS)
+    {
+        //Determine the reason for the error
+        char buildLog[BUILDLOGSIZE];
+        clGetProgramBuildInfo(program, device, CL_PROGRAM_BUILD_LOG, sizeof(buildLog), buildLog, NULL);
+        cerr << "Error in kernel: " << endl;
+        cerr << buildLog;
+        clReleaseProgram(program);
+        return NULL;
+    }
+
+    return program;
 }
 
 int main(int argc, char *argv[])
